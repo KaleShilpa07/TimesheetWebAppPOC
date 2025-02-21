7f81f29042c64a26509a67b81490f7e42f1f5ed2
KaleShilpa07 <kaleshilpa1716@gmail.com>
2025-02-21 12:27:56 +0530

    Removed timesheet_app folder

--- timesheet_app/src/reportWebVitals.js
+++
@@ -1,9 +0,0 @@
-import { report } from 'web-vitals';
-
-const reportWebVitals = (onPerfEntry) => {
-    if (onPerfEntry && onPerfEntry instanceof Function) {
-        report(onPerfEntry);
-    }
-};
-
-export default reportWebVitals;
